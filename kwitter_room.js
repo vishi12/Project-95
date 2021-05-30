0ad952155d090ef938cd468dbadc6418b43e4653
vishi12 <77247243+vishi12@users.noreply.github.com>
2021-05-30 16:42:00 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD-da_CDvt2FFoNQOSEyCsHPIjPWswYhEI",
+    authDomain: "project-46-6ed01.firebaseapp.com",
+    databaseURL: "https://project-46-6ed01-default-rtdb.firebaseio.com",
+    projectId: "project-46-6ed01",
+    storageBucket: "project-46-6ed01.appspot.com",
+    messagingSenderId: "809681368067",
+    appId: "1:809681368067:web:a9eb8bd002e8a054f1ca36",
+    measurementId: "G-GSD5615NVM"
+  };
+  // Initialize Firebase
